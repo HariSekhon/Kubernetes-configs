10408a6f44e90a9728f105c5fc33d218a2d95386
Hari Sekhon <harisekhon@gmail.com>
2022-02-08 15:46:59 +0000

    changed indentation to pass linting

--- setup/ci_bootstrap.sh
+++ setup/ci_bootstrap.sh
@@ -4,7 +4,7 @@
 #  Author: Hari Sekhon
 #  Date: 2020-06-02 17:43:35 +0100 (Tue, 02 Jun 2020)
 #
-#  https://github.com/HariSekhon/Kubernetes-Configs
+#  https://github.com/harisekhon/kubernetes-configs
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
